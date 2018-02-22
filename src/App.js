106c738016e0ca7721a20e96190317fd9fb38f11
Ruth Uwemedimo <emailtoruth@virginmedia.com>
2018-02-22 20:17:56 +0000

    add login amongst other things

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Login from "./Login.js";
+import { Button } from "semantic-ui-react";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       selected: null
-    }
+    };
     this.onClick = this.onClick.bind(this);
   }
 
@@ -16,18 +18,51 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <h2 className="heading">how are you today?</h2>
-        <div className="emoji-containter">
-          <p>
-            <span style={{ opacity: this.state.selected === 5 ? 1 : 0.2 }} onClick={() => this.onClick(5)}>😀 </span>
-            <span style={{ opacity: this.state.selected === 4 ? 1 : 0.2 }} onClick={() => this.onClick(4)}>🙂 </span>
-            <span style={{ opacity: this.state.selected === 3 ? 1 : 0.2 }} onClick={() => this.onClick(3)}>😐 </span>
-            <span style={{ opacity: this.state.selected === 2 ? 1 : 0.2 }} onClick={() => this.onClick(2)}>🙁 </span>
-            <span style={{ opacity: this.state.selected === 1 ? 1 : 0.2 }} onClick={() => this.onClick(1)}>☹️ </span>
-            <span style={{ opacity: this.state.selected === 0 ? 1 : 0.2 }} onClick={() => this.onClick(0)}>😔 </span>
-          </p>
+      <div className="full-height container">
+        <h2 style={{ position: "absolute", top: "40px", left: "40px" }}>
+          how are you today?
+        </h2>
+        <div className="vertical-align center-align emoji-container">
+          <span
+            style={{ opacity: this.state.selected === 5 ? 1 : 0.2 }}
+            onClick={() => this.onClick(5)}
+          >
+            😀{" "}
+          </span>
+          <span
+            style={{ opacity: this.state.selected === 4 ? 1 : 0.2 }}
+            onClick={() => this.onClick(4)}
+          >
+            🙂{" "}
+          </span>
+          <span
+            style={{ opacity: this.state.selected === 3 ? 1 : 0.2 }}
+            onClick={() => this.onClick(3)}
+          >
+            😐{" "}
+          </span>
+          <span
+            style={{ opacity: this.state.selected === 2 ? 1 : 0.2 }}
+            onClick={() => this.onClick(2)}
+          >
+            🙁{" "}
+          </span>
+          <span
+            style={{ opacity: this.state.selected === 1 ? 1 : 0.2 }}
+            onClick={() => this.onClick(1)}
+          >
+            ☹️{" "}
+          </span>
+          <span
+            style={{ opacity: this.state.selected === 0 ? 1 : 0.2 }}
+            onClick={() => this.onClick(0)}
+          >
+            😔{" "}
+          </span>
         </div>
+        <Button primary className="fixed-button">
+          Next
+        </Button>
       </div>
     );
   }
---
+++ src/Login.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { Grid, Form, Button } from "semantic-ui-react";
+
+class Login extends Component {
+  renderContent() {
+    return (
+      <div className="full-height">
+        <div className="vertical-align">
+          <h1 className="center-align">Please login</h1>
+          <Form>
+            <Form.Field>
+              <label>Username</label>
+              <input />
+            </Form.Field>
+            <Form.Field>
+              <label>Password</label>
+              <input type="password" />
+            </Form.Field>
+            <Button primary fluid type="submit">
+              Login
+            </Button>
+          </Form>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <Grid centered>
+        <Grid.Column width={4} only="computer">
+          {this.renderContent()}
+        </Grid.Column>
+        <Grid.Column width={6} only="tablet">
+          {this.renderContent()}
+        </Grid.Column>
+        <Grid.Column width={12} only="mobile">
+          {this.renderContent()}
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+export default Login;
